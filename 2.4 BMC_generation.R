56c7e5dd41741c0883cdd529f288ab80189ebaf2
linhchu-3 <linhkhanhchu3@gmail.com>
2026-06-18 13:44:17 -0400

    Update 2.4 BMC_generation.R

--- 2.4 BMC_generation.R
+++ 2.4 BMC_generation.R
@@ -4,7 +4,7 @@ library(dplyr)
 #choose cluster, use cluster 1 as example
 
 cluster_id <- 1
-dir <- paste0("Cluster", cluster_id)
+cluster_dir <- paste0("Cluster", cluster_id)
 
 #Extract the signed cor with FDR < 0.05 
 #example, mincell cutoff = 40 for the cluster
@@ -12,7 +12,7 @@ dir <- paste0("Cluster", cluster_id)
 for (cutoff in 40) {
   
   # Construct file name
-  rds_file <- file.path(dir, paste0("Cluster3/ijw_FDR_0.05_pos_neg_mincell", cutoff, ".rds")
+  rds_file <- file.path(cluster_dir, paste0("ijw_FDR_0.05_pos_neg_mincell", cutoff, ".rds")
   
   # Load the RDS file
   ijw_FDR <- readRDS(rds_file)
@@ -30,8 +30,8 @@ for (cutoff in 40) {
   }
 
 #Extract mean value
-Mean_list <- readRDS(paste0(dir,"PDControl_avg_signed_Mean_list.rds"))
-MSE_list <- readRDS(paste0(dir, "PDControl_avg_signed_MSE_list.rds"))
+Mean_list <- readRDS(paste0(cluster_dir,"PDControl_avg_signed_Mean_list.rds"))
+MSE_list <- readRDS(paste0(cluster_dir, "PDControl_avg_signed_MSE_list.rds"))
 
 Mean <- Mean_list[["40"]]
 
@@ -42,4 +42,4 @@ ijw_FDR_0.05_substract <- ijw_FDR_0.05_subset_mincell40 %>%
 #Calculate new rho - BMC
 BMC <- ijw_FDR_0.05_substract
 
-saveRDS(BMC, paste0(dir, "BMC.RDS")
+saveRDS(BMC, paste0(cluster_dir, "BMC.RDS")
